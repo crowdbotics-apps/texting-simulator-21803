8a9b4a5ff6c946ce1ae77929d9fc32a6b4bab326
Crowdbotics <team@crowdbotics.com>
2020-10-21 20:44:58 +0000

    Initial commit.

---
+++ ios/texting_simulator_21803Tests/texting_simulator_21803Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface texting_simulator_21803 : XCTestCase
+
+@end
+
+@implementation texting_simulator_21803
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
